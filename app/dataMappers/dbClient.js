b377cbf102e34c8adbab484b34fa8710a9938cff
robin-plancon <robin.plancon@outlook.com>
2023-09-28 13:41:14 +0200

    change pg connection

--- app/dataMappers/dbClient.js
+++ app/dataMappers/dbClient.js
@@ -1,7 +1,13 @@
 const { Client } = require("pg");
 require('dotenv').config()
 
-const client = new Client(process.env.PG_URL)
+const client = new Client({
+	host: process.env.PG_HOST,
+	user: process.env.PG_USER,
+	password: process.env.PG_PASSWORD,
+	database: process.env.PG_DATABASE,
+	port: process.env.PG_PORT,
+})
 
 client.connect()
 
