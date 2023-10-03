8bd4a77a6e41f05ffc0fb1c5f93d360b6c68e0fd
vanessaslama <vanessa.slama@oclock.school>
2023-10-03 13:49:07 +0200

    feat: Added random games selection

--- app/dataMappers/gameDataMapper.js
+++ app/dataMappers/gameDataMapper.js
@@ -1,11 +1,28 @@
 const axios = require('axios');
 require('dotenv').config();
+const gameData = require('../data/gameData.json');
 
 
 const gameDataMapper = {
   
   // Retrieve all games from specific list of platforms from IGDB API
   getGames: async function () {
+    
+    const randomIdsArray= []
+    
+    for (const id of gameData.gameIds) {
+      if (randomIdsArray.length >= 500) {
+        break;
+      }
+      const randomNumber= Math.floor( Math.random()*(gameData.gameIds.length));
+      const randomApiId= gameData.gameIds[randomNumber];
+      const checkDuplicate= randomIdsArray.some((id) => id == randomApiId ); 
+      if (!checkDuplicate) {
+        randomIdsArray.push(randomApiId) 
+      }
+    }
+    console.log("Le tableau avant requête :",randomIdsArray)
+    const randomIdsList= randomIdsArray.join()
     try{
       const result = await axios(
         
@@ -16,7 +33,7 @@ const gameDataMapper = {
           'Client-ID': process.env.PG_CLIENT_ID,
           'Authorization': process.env.PG_AUTHORIZATION,
         },
-        data: "fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; where platforms = (4, 7, 15, 16, 18, 19, 22, 25, 26, 27, 29, 30, 32, 33, 35, 50, 51, 53, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71, 75, 78, 79, 80, 84, 86, 87, 88, 89, 90, 93, 94, 99, 114, 115, 117, 119, 120, 123, 128, 136, 142, 154, 158, 274, 373, 410); limit 100;"
+        data: `fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; where platforms = (4, 7, 15, 16, 18, 19, 22, 25, 26, 27, 29, 30, 32, 33, 35, 50, 51, 53, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71, 75, 78, 79, 80, 84, 86, 87, 88, 89, 90, 93, 94, 99, 114, 115, 117, 119, 120, 123, 128, 136, 142, 154, 158, 274, 373, 410) & id=(${randomIdsList}); limit 500;`
       })
       
       return result.data;
@@ -26,7 +43,7 @@ const gameDataMapper = {
     }
     
   },
-
+  
   // Retrieve one game by its ID with details from IGDB API
   getOneGame: async function (id) {
     try{
@@ -69,29 +86,29 @@ const gameDataMapper = {
     catch (error) {
       console.log(error);
     }
-},
-
-// Retrieve games by their platform from IGDB API
-getGamesByPlatform : async function (platformId) {
-  try {
-    const result = await axios(
-      
-      { method: 'POST',
-      url: "https://api.igdb.com/v4/games",
-      headers: {
-        'Accept': 'application/json',
-        'Client-ID': process.env.PG_CLIENT_ID,
-        'Authorization': process.env.PG_AUTHORIZATION,
-      },
-      data: `fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; where platforms = ${platformId};`
-    })
-    console.log(result.data)
-    return result.data;
-  }
-  catch (error) {
-    console.log(error);
-  }
-},
+  },
+  
+  // Retrieve games by their platform from IGDB API
+  getGamesByPlatform : async function (platformId) {
+    try {
+      const result = await axios(
+        
+        { method: 'POST',
+        url: "https://api.igdb.com/v4/games",
+        headers: {
+          'Accept': 'application/json',
+          'Client-ID': process.env.PG_CLIENT_ID,
+          'Authorization': process.env.PG_AUTHORIZATION,
+        },
+        data: `fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; where platforms = ${platformId};`
+      })
+      console.log(result.data)
+      return result.data;
+    }
+    catch (error) {
+      console.log(error);
+    }
+  },
   
 };
 
