67c7ba7672cfb9b21abeb652b2d5b04714070708
LionelBouldoyre <lionel.bouldoyre@oclock.school>
2023-10-04 11:42:11 +0200

    feat: added game boy advance games

--- app/service/redisClient.js
+++
@@ -1,7 +0,0 @@
-const { createClient } = require('redis');
-
-// Initialize client.
-let redisClient = createClient();
-redisClient.connect().catch(console.error);
-
-module.exports = redisClient;
--- index.js
+++ index.js
@@ -3,15 +3,9 @@ require('dotenv').config();
 const express = require('express');
 const router = require('./app/router');
 const session = require('express-session');
-const RedisStore = require("connect-redis").default;
 const notFound404 = require("./app/middlewares/notFound404");
 const cors = require('cors');
 
-const redisClient = require("./app/service/redisClient");
-let redisStore = new RedisStore({
-  client: redisClient,
-  prefix: "retrOgame :", // le prefix va permettre de classer les clefs qui concernent notre application
-});
 
 const app = express();
 app.use(cors(
@@ -29,7 +23,6 @@ app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", "./app/views");
 app.use(session({
-  store: redisStore,
   secret: process.env.PG_SESSION_SECRET,
   resave: true, 
   saveUninitialized: false,
