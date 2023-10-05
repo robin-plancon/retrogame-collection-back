bbf5e61a839681c7029c4a57ae598bbeca3d0d85
Florian CORLU <florian.corlu@hotmail.fr>
2023-10-05 14:17:40 +0200

    fix: conflict/adding Game boy advance in platform list

--- app/dataMappers/gameDataMapper.js
+++ app/dataMappers/gameDataMapper.js
@@ -100,11 +100,7 @@ const gameDataMapper = {
           'Client-ID': process.env.PG_CLIENT_ID,
           'Authorization': process.env.PG_AUTHORIZATION,
         },
-<<<<<<< HEAD
-        data: `fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; search "${game}"; where platforms = (4, 7, 15, 16, 18, 19, 22, 25, 26, 27, 29, 30, 32, 33, 35, 50, 51, 53, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71, 75, 78, 79, 80, 84, 86, 87, 88, 89, 90, 93, 94, 99, 114, 115, 117, 119, 120, 123, 128, 136, 142, 154, 158, 274, 373, 410); limit 500;`
-=======
-        data: `fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; search "${game}"; where platforms = (4, 7, 15, 16, 18, 19, 22, 24, 25, 26, 27, 29, 30, 32, 33, 35, 50, 51, 53, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71, 75, 78, 79, 80, 84, 86, 87, 88, 89, 90, 93, 94, 99, 114, 115, 117, 119, 120, 123, 128, 136, 142, 154, 158, 274, 373, 410);`
->>>>>>> Forgotten-password
+        data: `fields id, cover.url, name, slug, first_release_date, genres.name, platforms.name, platforms.platform_logo.url, screenshots.url, summary; search "${game}"; where platforms = (4, 7, 15, 16, 18, 19, 22, 24, 25, 26, 27, 29, 30, 32, 33, 35, 50, 51, 53, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 70, 71, 75, 78, 79, 80, 84, 86, 87, 88, 89, 90, 93, 94, 99, 114, 115, 117, 119, 120, 123, 128, 136, 142, 154, 158, 274, 373, 410);limit 500;`
       })
       console.log(result.data.map(game => game.name));
       return result.data;
