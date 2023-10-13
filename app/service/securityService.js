8f5f6fe0f9fc4576710c50f87927adac5878091e
Robin Plancon <robin.plancon@outlook.com>
2023-10-13 09:48:42 +0200

    debug session

--- app/service/securityService.js
+++ app/service/securityService.js
@@ -12,6 +12,8 @@ const securityService = {
         const token = req.headers.authorization.split(" ")[1]; 
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = req.session.user
+        console.log('user', user);
+        console.log('decoded', decoded);
         
         // We compare the user's data inside the token with the data from the session before allowing access to the user 
         if (user.nickname == decoded.nickname && user.mail == decoded.mail) {
